3695fd395c65711a04162ca3e91f67c84659c122
Kakho <giokaxura@gmail.com>
2020-01-05 19:14:23 +0400

    for merge

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
     $('.sauce').click(function () {
         $(this).parent('.actions').find('.hidden_more').toggleClass('show');
     });
+
     $('.nav_button').click(function () {
         $(".menu").addClass('show');
         $('body').addClass('hidden');
@@ -28,7 +29,7 @@ $(document).ready(function () {
     var swiper = new Swiper('.swiper-container', {
         slidesPerView: 'auto',
         spaceBetween: 16,
-        loop: true,
+        loop: $('.main-slider-wrapper').children().length > 5 ? true : false,
         grabCursor: true,
         autoplay: {
             delay: 3000,
@@ -126,9 +127,7 @@ $(document).ready(function () {
         if ($('.hidden_more').hasClass('show') && n.target.closest(".sauce") == null) {
             changeText();
         }
-        if ($('.hidden_cards').hasClass('show') && n.target.closest(".choose_card") == null) {
-            changeCard();
-        }
+
         if ($('.category').hasClass('border') && n.target.closest(".category") == null) {
             $('.category').removeClass('border');
         }
@@ -153,31 +152,6 @@ $(document).ready(function () {
         $('.hidden_more.show').removeClass('show');
         $('.sauce.active').removeClass('active');
     }
-
-
-    //  Alert
-
-    $('.check_cart').click(function () {
-        let check = false;
-        let actions = $(this).closest('.actions');
-        actions.find('input[type=radio]').each(function () {
-            if (this.checked) {
-                check = true;
-            }
-        });
-        if (!check) {
-            actions.find('.sauce').addClass('bounce');
-            actions.closest('.bottom_container').siblings('.top_container').find('.alert').addClass('show')
-            setTimeout(function () {
-                actions.find('.sauce').removeClass('bounce');
-            }, 700)
-            setTimeout(function () {
-                $('.alert').removeClass('show');
-            }, 2000)
-        } else {
-            $(".successfully_add").addClass('show');
-        }
-    });
 
     // Count
 
@@ -252,11 +226,12 @@ $(document).ready(function () {
         }
     });
 
-    // modals 
+    // modals
     function openModal(target){
         $("#"+target).addClass('show_modal');
         $('body').addClass('hidden');
     }
+
     function colseModal(){
         $(".modal").removeClass('show_modal');
         $('body').removeClass('hidden');
@@ -287,18 +262,18 @@ $(document).ready(function () {
         colseModal();
     });
 
-    $('.deleteProducts').click(function () {
-        $('.products_table tbody').fadeOut();
-        setTimeout(function () {
-            $('.products_table tbody').remove();
-        }, 3000);
-        colseModal();
-    });
+    // $('.deleteProducts').click(function () {
+    //     $('.products_table tbody').fadeOut();
+    //     setTimeout(function () {
+    //         $('.products_table tbody').remove();
+    //     }, 3000);
+    //     colseModal();
+    // });
 
     $('.now').click(function () {
         colseModal();
     });
-    
+
 
     // Order
 
@@ -327,15 +302,15 @@ $(document).ready(function () {
         }
     }
 
-    // cart delete item  
-    
-    $('.products_table .remove').click(function () {
-        let item = $(this).closest("tr")
-        item.fadeOut();
-        setTimeout(function () {
-            item.remove();
-        }, 3000);
-    });
+    // cart delete item
+
+    // $('.products_table .remove').click(function () {
+    //     let item = $(this).closest("tr")
+    //     item.fadeOut();
+    //     setTimeout(function () {
+    //         item.remove();
+    //     }, 3000);
+    // });
 
     $('table .remove').click(function () {
         let item = $(this).closest("tr")
@@ -363,92 +338,75 @@ $(document).ready(function () {
     /*look for any elements with the class "custom-select":*/
     x = document.getElementsByClassName("custom-select");
     for (i = 0; i < x.length; i++) {
-    selElmnt = x[i].getElementsByTagName("select")[0];
-    /*for each element, create a new DIV that will act as the selected item:*/
-    a = document.createElement("DIV");
-    a.setAttribute("class", "select-selected");
-    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-    x[i].appendChild(a);
-    /*for each element, create a new DIV that will contain the option list:*/
-    b = document.createElement("DIV");
-    b.setAttribute("class", "select-items select-hide");
-    for (j = 1; j < selElmnt.length; j++) {
-        /*for each option in the original select element,
-        create a new DIV that will act as an option item:*/
-        c = document.createElement("DIV");
-        c.innerHTML = selElmnt.options[j].innerHTML;
-        c.addEventListener("click", function(e) {
-            /*when an item is clicked, update the original select box,
-            and the selected item:*/
-            var y, i, k, s, h;
-            s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-            h = this.parentNode.previousSibling;
-            for (i = 0; i < s.length; i++) {
-            if (s.options[i].innerHTML == this.innerHTML) {
-                s.selectedIndex = i;
-                h.innerHTML = this.innerHTML;
-                y = this.parentNode.getElementsByClassName("same-as-selected");
-                for (k = 0; k < y.length; k++) {
-                y[k].removeAttribute("class");
-                }
-                this.setAttribute("class", "same-as-selected");
-                break;
+        selElmnt = x[i].getElementsByTagName("select")[0];
+        if (selElmnt.getElementsByTagName("option").length > 0) {
+            /*for each element, create a new DIV that will act as the selected item:*/
+            a = document.createElement("DIV");
+            a.setAttribute("class", "select-selected");
+            a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+            x[i].appendChild(a);
+            /*for each element, create a new DIV that will contain the option list:*/
+            b = document.createElement("DIV");
+            b.setAttribute("class", "select-items select-hide");
+            for (j = 1; j < selElmnt.length; j++) {
+                /*for each option in the original select element,
+                create a new DIV that will act as an option item:*/
+                c = document.createElement("DIV");
+                c.innerHTML = selElmnt.options[j].innerHTML;
+                c.addEventListener("click", function(e) {
+                    /*when an item is clicked, update the original select box,
+                    and the selected item:*/
+                    var y, i, k, s, h;
+                    s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+                    h = this.parentNode.previousSibling;
+                    for (i = 0; i < s.length; i++) {
+                    if (s.options[i].innerHTML == this.innerHTML) {
+                        s.selectedIndex = i;
+                        h.innerHTML = this.innerHTML;
+                        y = this.parentNode.getElementsByClassName("same-as-selected");
+                        for (k = 0; k < y.length; k++) {
+                        y[k].removeAttribute("class");
+                        }
+                        this.setAttribute("class", "same-as-selected");
+                        break;
+                    }
+                    }
+                    h.click();
+                    changeDistrict(s);
+                });
+                b.appendChild(c);
             }
-            }
-            h.click();
-        });
-        b.appendChild(c);
-    }
-    x[i].appendChild(b);
-    a.addEventListener("click", function(e) {
-        /*when the select box is clicked, close any other select boxes,
-        and open/close the current select box:*/
-        e.stopPropagation();
-        closeAllSelect(this);
-        this.nextSibling.classList.toggle("select-hide");
-        this.classList.toggle("select-arrow-active");
-        });
+            x[i].appendChild(b);
+            a.addEventListener("click", function(e) {
+                /*when the select box is clicked, close any other select boxes,
+                and open/close the current select box:*/
+                e.stopPropagation();
+                closeAllSelect(this);
+                this.nextSibling.classList.toggle("select-hide");
+                this.classList.toggle("select-arrow-active");
+            });
+        }
     }
     function closeAllSelect(elmnt) {
-    /*a function that will close all select boxes in the document,
-    except the current select box:*/
-    var x, y, i, arrNo = [];
-    x = document.getElementsByClassName("select-items");
-    y = document.getElementsByClassName("select-selected");
-    for (i = 0; i < y.length; i++) {
-        if (elmnt == y[i]) {
-        arrNo.push(i)
-        } else {
-        y[i].classList.remove("select-arrow-active");
+        /*a function that will close all select boxes in the document,
+        except the current select box:*/
+        var x, y, i, arrNo = [];
+        x = document.getElementsByClassName("select-items");
+        y = document.getElementsByClassName("select-selected");
+        for (i = 0; i < y.length; i++) {
+            if (elmnt == y[i]) {
+            arrNo.push(i)
+            } else {
+                y[i].classList.remove("select-arrow-active");
+            }
         }
-    }
-    for (i = 0; i < x.length; i++) {
-        if (arrNo.indexOf(i)) {
-        x[i].classList.add("select-hide");
+        for (i = 0; i < x.length; i++) {
+            if (arrNo.indexOf(i)) {
+                x[i].classList.add("select-hide");
+            }
         }
-    }
     }
     /*if the user clicks anywhere outside the select box,
     then close all select boxes:*/
     document.addEventListener("click", closeAllSelect);
-
-
-    $('.choose_card').click(function () {
-        $(this).toggleClass('active');
-    });
-    $('.choose_card').click(function () {
-        $(this).parent('.choose_or').find('.hidden_cards').toggleClass('show');
-    });
-
-    $('.card>.container').click(function () {
-        changeCard(this.innerText);
-    });
-
-    function changeCard(str = null) {
-        if (str) {
-            $('.choose_card.active p').text(str.length > 20 ? str.slice(0, 20) + "..." : str);
-        }
-        $('.hidden_cards.show').removeClass('show');
-        $('.choose_card.active').removeClass('active');
-    }
 });
